fd5d9d285ca537ccc1e9f0fd7c53a49011bb5df4
Arthur Fornaciari Gaviorno <103372834+arthurgaviorno@users.noreply.github.com>
2023-04-28 10:53:39 -0300

    Add files via upload
    APP..JS ADICIONADO RECENTEMENTE É O DA AULA!!!!

--- App.js
+++ App.js
@@ -2,51 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { Text, View, Image} from 'react-native';
 
-import styles from './src/components/styles'
+import CardFilmes from './CardFilmes';
+
+const baseURL = 'https://api.otaviolube.com';
+
 
 
 export default function App() {
 
-  let [filmes, setFilmes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
 
-  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
-  const imageURL = 'https://api.otaviolube.com';
+
 
   useEffect(function(){
-    fetch(baseURL)
-    .then(data => data.json())
+    fetch('https://api.otaviolube.com/api/filmes?populate=*')
+    .then(response => response.json())
     .then(objeto => {
       console.log(objeto);
       setFilmes(objeto.data);
     })
-  }, []);
+  }, [])
 
   return (
 
   <View style={styles.container}>
+    {filmes.map(filme => 
+      <CardFilmes filme = {filme.attributes} baseURL = {baseURL}/>
     
-    {filmes.length > 0 ? (
-      filmes.map((filme) => (
-        <View key={filme._id} style={styles.content}>
-          <View style={styles.cardFilmes}>
-
-            <Text style={styles.anuncio}>EM EXIBIÇÃO NOS CINEMAS MAIS PRÓXIMOS!</Text>
-
-              <View style={styles.Img}>
-                  <Image style={styles.poster} source={{uri: imageURL+filme.attributes.poster.data.attributes.formats.small.url}} ></Image>
-               </View> 
-
-            <Text style={styles.titulo}>{filme.attributes.titulo}</Text>
-            <Text style={styles.subtitulo}>{filme.attributes.subtitulo}</Text>
-            <Text style={styles.sinopse}>{filme.attributes.sinopse}</Text>
-
-          </View>
-        </View>
-      ))
-      
-    ) : (
-      <Text>Aguarde o catálogo</Text>
     )}
+    
     <StatusBar style="auto" />
   </View>
 );
