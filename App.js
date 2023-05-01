60ae4f8559a629c91bd7d1045a7b4477314e0552
Arthur Fornaciari Gaviorno <103372834+arthurgaviorno@users.noreply.github.com>
2023-04-30 21:55:11 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Text, View, Image} from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
 
-import CardFilmes from './CardFilmes';
-
-const baseURL = 'https://api.otaviolube.com';
-
-
+import styles from './src/components/styles'
+import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
 
-  const [filmes, setFilmes] = useState([]);
+  let [filmes, setFilmes] = useState([]);
 
-
+  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
+  const imageURL = 'https://api.otaviolube.com';
 
   useEffect(function(){
-    fetch('https://api.otaviolube.com/api/filmes?populate=*')
-    .then(response => response.json())
+    fetch(baseURL)
+    .then(data => data.json())
     .then(objeto => {
       console.log(objeto);
       setFilmes(objeto.data);
     })
-  }, [])
+  }, []);
 
   return (
 
-  <View style={styles.container}>
-    {filmes.map(filme => 
-      <CardFilmes filme = {filme.attributes} baseURL = {baseURL}/>
-    
+  <View style={styles.content}>
+
+    {(filmes.map((filme) => (
+
+      <SafeAreaView key={filme._id} style={styles.container}>
+
+        <ScrollView>
+      
+        <View style={styles.cardFilmes}>
+          <Text style={styles.anuncio}>EM EXIBIÇÃO NOS CINEMAS MAIS PRÓXIMOS!</Text>
+          <View style={styles.Img}>
+              <Image style={styles.poster} source={{uri: imageURL+filme.attributes.poster.data.attributes.formats.small.url}}></Image>
+          </View>
+          <Text style={styles.titulo}>{filme.attributes.titulo}</Text>
+          <Text style={styles.subtitulo}>{filme.attributes.subtitulo}</Text>
+          <Text style={styles.sinopse}>{filme.attributes.sinopse}</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Alugar Filme</Text>
+          </TouchableOpacity>
+        </View>
+        </ScrollView>
+      </SafeAreaView>
+    ))
     )}
     
     <StatusBar style="auto" />
   </View>
-);
 
-}
-
+  )}
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,69 +1,79 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    anuncio: {
-        marginBottom: 10,
-        fontSize: 15,
-        fontWeight: 'bold',
-    },
+  anuncio: {
+    margin: 5,
+    fontSize: 15,
+    fontWeight: 'bold',
+    
+},
 
-    cardFilmes: {
-      display: 'flex',
-      paddingLeft: '10%',
-      flexDirection: 'column',
-      backgroundColor: "white",
-      alignItems: 'center',
-      justifyContent: 'center',
-      maxWidth: 720
-    },
+container: {
+    alignItems: "center",
+    justifyContent: "center",
     
-    viewImg: {
-      padding: 3,
-      width: '70%',
-      height: '50%',
-      maxWidth: '100%',
-      maxHeight: 550,
-      display: 'flex',
-      justifyContent: 'center'
-    },
-  
-    content: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      fontSize: 18,
-      margin: 10,
-    },
-    
-    titulo: {
-      marginTop: '10%',
-      fontSize: 20,
-      color: 'black',
-      fontWeight: 'bold',
-      marginBottom: 3,
-      textAlign: 'center',
-    },
-    
-    subtitulo: {
-      fontSize: 15,
-      color: 'black',
-      fontWeight: 'bold',
-      fontStyle: 'italic',
-      textAlign: 'center'
-    },
-    
-    sinopse: {
-      color: 'black',
-      textAlign: 'center',
-      marginBottom: '10%',
-      maxWidth: 390
-    },
-  
-    poster: {
-      width: 350,
-      height: 700,
-    }
-  });
+},
+
+cardFilmes: {
+  backgroundColor: "red",
+  alignItems: "center",
+  justifyContent: "center",
+  maxWidth: 400,
+  margin: 15,
+  borderRadius: 15
+
+},
+
+content: {
+ width: '100%',
+},
+
+titulo: {
+  marginTop: '5%',
+  fontSize: 20,
+  color: 'black',
+  fontWeight: 'bold',
+  marginBottom: 3,
+  textAlign: 'center',
+},
+
+subtitulo: {
+  fontSize: 15,
+  color: 'black',
+  fontWeight: 'bold',
+  fontStyle: 'italic',
+  textAlign: 'center',
+  marginBottom: 5
+},
+
+sinopse: {
+  color: 'black',
+  textAlign: 'center',
+  marginBottom: '5%',
+  maxWidth: 390
+
+},
+
+poster: {
+  width: 250,
+  height: 400,
+},
+
+button: {
+  height: 40,
+  width: 300,
+  borderRadius: 25,
+  backgroundColor: 'black',
+  margin: 10,
+  alignItems: 'center',
+  justifyContent: 'center'
+},
+
+buttonText: {
+  color: 'white',
+  fontWeight: 'bold'
+}
+
+});
 
   export default styles;
